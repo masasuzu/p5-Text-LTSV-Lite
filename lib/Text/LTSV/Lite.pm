ac0db18e7ea28a66dbf40e7eba21dd3ba22a719c
SUZUKI Masashi <m15.suzuki.masashi@gmail.com>
2013-04-27 21:11:44 +0900

    initial commit

---
+++ lib/Text/LTSV/Lite.pm
@@ -0,0 +1,32 @@
+package Text::LTSV::Lite;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Text::LTSV::Lite -
+
+=head1 SYNOPSIS
+
+  use Text::LTSV::Lite;
+
+=head1 DESCRIPTION
+
+Text::LTSV::Lite is
+
+=head1 AUTHOR
+
+SUZUKI Masashi E<lt>m15.suzuki.masashi {at} gmail.ocomE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
